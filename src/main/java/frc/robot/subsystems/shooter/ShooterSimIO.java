c7b82137f41bf691fcca17140729480004933409
Eilon huppert <eilonhuppert@gmail.com>
2026-01-31 16:40:10 +0200

    the simulation is still wired but less wired

--- src/main/java/frc/robot/subsystems/shooter/ShooterSimIO.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterSimIO.java
@@ -5,27 +5,21 @@ import static frc.robot.subsystems.shooter.ShooterConstants.ShooterSimConstants.
 
 import com.ctre.phoenix6.StatusSignal;
 import edu.wpi.first.math.MathUtil;
-import edu.wpi.first.wpilibj.RobotController;
-import edu.wpi.first.wpilibj.simulation.BatterySim;
-import edu.wpi.first.wpilibj.simulation.RoboRioSim;
 
 public class ShooterSimIO implements ShooterIO {
 
   public ShooterSimIO() {
-    _masterMotor.getConfigurator().apply(realConfigs); // ^ right now this is real configs
+    _masterMotor.getConfigurator().apply(simConfigs); // ^ right now this is real configs
     _followerMotor.setControl(followerRequest);
   }
 
   @Override
   public void updateInputs(ShooterIOInputs inputs) {
     // gets deta
-    simMotor.setSupplyVoltage(RobotController.getBatteryVoltage());
+    simMotor.setSupplyVoltage(12);
     sim.setInputVoltage(MathUtil.clamp(simMotor.getMotorVoltage(), -12.0, 12.0));
     sim.update(0.02);
-
-    simMotor.setRotorVelocity(sim.getAngularVelocityRadPerSec() / GEAR_RATIO);
-    RoboRioSim.setVInVoltage(
-        BatterySim.calculateDefaultBatteryLoadedVoltage(sim.getCurrentDrawAmps()));
+    simMotor.setRotorVelocity(sim.getAngularVelocity().times(GEAR_RATIO));
 
     StatusSignal.refreshAll(
         currentVoltage,
@@ -36,7 +30,7 @@ public class ShooterSimIO implements ShooterIO {
     inputs.voltage = currentVoltage.getValueAsDouble();
     inputs.statorCurrent = currentStatorCurrent.getValueAsDouble();
     inputs.supplyCurrent = sim.getCurrentDrawAmps();
-    inputs.velocity = currentVelocity.getValueAsDouble() * GEAR_RATIO;
+    inputs.velocity = currentVelocity.getValueAsDouble();
     inputs.acceleration = currentAcceleration.getValueAsDouble();
     inputs.wantedVelocity = targetVelocity;
   }
