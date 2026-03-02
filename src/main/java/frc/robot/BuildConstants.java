6472acfe3fa111fbcb7e9d9c2ee209c2786ac3ef
Uri <138823597+Battlepants33@users.noreply.github.com>
2026-03-02 16:47:04 +0200

    build comstants

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+/** Automatically generated file containing build version information. */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "Rebuilt-2026-robot-code";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 39;
+  public static final String GIT_SHA = "744c1839f2e87795c9d9186c2f4151373e9297a0";
+  public static final String GIT_DATE = "2026-02-23 07:09:12 EST";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2026-02-23 14:53:38 EST";
+  public static final long BUILD_UNIX_TIME = 1771876418550L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants() {}
+}
