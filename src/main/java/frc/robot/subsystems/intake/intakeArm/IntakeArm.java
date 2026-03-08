5e537126ac11610705a89939c82f40ed4f76a2c8
Uri <138823597+Battlepants33@users.noreply.github.com>
2026-03-08 17:46:24 +0200

    intake arm command fixes + javadocs

--- src/main/java/frc/robot/subsystems/intake/intakeArm/IntakeArm.java
+++ src/main/java/frc/robot/subsystems/intake/intakeArm/IntakeArm.java
@@ -7,6 +7,7 @@ package frc.robot.subsystems.intake.intakeArm;
 import static edu.wpi.first.units.Units.*;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import org.littletonrobotics.junction.Logger;
@@ -56,31 +57,67 @@ public class IntakeArm extends SubsystemBase {
     io.resetPosition();
   }
   // Commands
+  /**
+   * @return a command that sets the voltage of the intake arm motor. The motor will be stopped when the command ends. 
+   */
   public Command setVoltage(double voltage) {
     return startEnd(() -> io.setVoltage(voltage), io::stopMotor)
         .withName(getName() + "Set voltage");
   }
 
+  /**
+   * @return Set the voltage of the intake arm motor without stopping it when the command ends. 
+   */
   public Command setVoltageNoStop(double voltage) {
     return startEnd(() -> io.setVoltage(voltage), io::stopMotor)
         .withName(getName() + "Set voltage");
   }
 
+  /**
+   * @return a command that sets the current of the intake arm motor. The motor will be stopped when the command ends.
+   */
   public Command setCurrent(double current) {
     return startEnd(() -> io.setCurrent(current), io::stopMotor)
         .withName(getName() + "Set current");
   }
 
-  public Command setPosition(double position) {
+  /**
+   * @return a command that sets the position of the intake arm.
+   */
+  public Command setPositionCommand(double position) {
     return runOnce(() -> io.setPosition(position)).withName(getName() + "Set position");
   }
 
-  public Command open(){
-    return setPosition(OPEN_POSITION);
+  /**
+   * @return a command that opens the intake arm. 
+   */
+  public Command openCommand(){
+    return setPositionCommand(OPEN_POSITION);
   }
 
+  /**
+   * @return a command that closes the intake arm.
+   */
+  public Command closeCommand(){
+    return setPositionCommand(CLOSE_POSITION);
+  }
+
+  /**
+   * @return a command that repeatedly opens and closes the intake arm with a 1 second delay in between. The intake arm will be left open when the command ends. for shoot commands.
+   */
+  public Command openAndCloseCommand(){
+    return Commands.repeatingSequence(
+      openCommand(),
+      Commands.waitSeconds(1),
+      closeCommand(),
+      Commands.waitSeconds(1)).finallyDo(() -> io.setPosition(OPEN_POSITION)).withName(getName() + "Open and Close");
+  }
+
+  /**
+   * @return a command that resets the position of the intake arm. for manual resetting. 
+   */
   public Command resetPositionCommand(){
-    return setVoltage(RESET_VOLTAGE).finallyDo(() -> setPosition(0));
+    return setVoltage(RESET_VOLTAGE).finallyDo(() -> io.resetPosition());
   }
 
   // sysId Commands
