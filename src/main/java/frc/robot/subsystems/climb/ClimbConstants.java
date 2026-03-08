5a0ae33e5b4a7b9e99d6a3f9c759d5a7a85b645e
Eilon huppert <eilonhuppert@gmail.com>
2026-03-08 19:19:14 +0200

    added sim constants

--- src/main/java/frc/robot/subsystems/climb/ClimbConstants.java
+++ src/main/java/frc/robot/subsystems/climb/ClimbConstants.java
@@ -1,48 +1,56 @@
 package frc.robot.subsystems.climb;
 
 import com.ctre.phoenix6.StatusSignal;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.PositionVoltage;
 import com.ctre.phoenix6.controls.TorqueCurrentFOC;
 import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+import com.ctre.phoenix6.sim.TalonFXSimState;
 
+import edu.wpi.first.math.system.LinearSystem;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.system.plant.LinearSystemId;
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.AngularAcceleration;
 import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.units.measure.Current;
 import edu.wpi.first.units.measure.Voltage;
+import edu.wpi.first.wpilibj.simulation.ElevatorSim;
 
 public class ClimbConstants {
-    // ids
-    public static final int id = 0; // TODO: find Id
+  // ids
+  public static final int id = 0; // TODO: find Id
 
-    // devices
-    public static final TalonFX _motor = new TalonFX(id);
+  // devices
+  public static final TalonFX _motor = new TalonFX(id);
 
-    // signals
-    public static final StatusSignal<Voltage> voltageSignal = _motor.getMotorVoltage();
-    public static final StatusSignal<Current> statorCurrentSignal = _motor.getStatorCurrent();
-    public static final StatusSignal<Current> supplyCurrentSignal = _motor.getSupplyCurrent();
-    public static final StatusSignal<Angle> postiton = _motor.getPosition();
-    public static final StatusSignal<AngularVelocity> velocitySignal = _motor.getVelocity();
-    public static final StatusSignal<AngularAcceleration> AccelerationSignal = _motor.getAcceleration();
+  // signals
+  public static final StatusSignal<Voltage> voltageSignal = _motor.getMotorVoltage();
+  public static final StatusSignal<Current> statorCurrentSignal = _motor.getStatorCurrent();
+  public static final StatusSignal<Current> supplyCurrentSignal = _motor.getSupplyCurrent();
+  public static final StatusSignal<Angle> postiton = _motor.getPosition();
+  public static final StatusSignal<AngularVelocity> velocitySignal = _motor.getVelocity();
+  public static final StatusSignal<AngularAcceleration> AccelerationSignal = _motor.getAcceleration();
 
-    // requests
-    public static final TorqueCurrentFOC currentRequest = new TorqueCurrentFOC(0);
-    public static final PositionVoltage positionRequest = new PositionVoltage(0);
-    public static final VelocityVoltage velocityRequest = new VelocityVoltage(0);
+  // requests
+  public static final TorqueCurrentFOC currentRequest = new TorqueCurrentFOC(0);
+  public static final PositionVoltage positionRequest = new PositionVoltage(0);
+  public static final VelocityVoltage velocityRequest = new VelocityVoltage(0);
 
-    // configs
-    public static final double STATOR_CURRENT = 100; // TODO: TUNE
-    public static final double SUPPLY_CURRENT = 50; // TODO: TUNE
-    public static final double GEAR_RATIO = 0; // TODO: TUNE
-    public static final NeutralModeValue NEUTRAL_MODE = NeutralModeValue.Brake; // TODO: TUNE
-    public static final InvertedValue INVERTED = InvertedValue.CounterClockwise_Positive; // TODO: TUNE
+  // configs
+  public static final double STATOR_CURRENT = 100; // TODO: TUNE
+  public static final double SUPPLY_CURRENT = 50; // TODO: TUNE
+  public static final double GEAR_RATIO = 1; // TODO: TUNE
+  public static final NeutralModeValue NEUTRAL_MODE = NeutralModeValue.Brake; // TODO: TUNE
+  public static final InvertedValue INVERTED = InvertedValue.CounterClockwise_Positive; // TODO: TUNE
 
-    // config declaration
-    public static final TalonFXConfiguration realConfiguration = new TalonFXConfiguration();
+  // config declaration
+  public static final TalonFXConfiguration realConfiguration = new TalonFXConfiguration();
 
-    static {
+  static {
 
     // peaks:
     realConfiguration.CurrentLimits.StatorCurrentLimitEnable = true;
@@ -65,59 +73,49 @@ public class ClimbConstants {
     realConfiguration.Slot0.kV = 0;
     realConfiguration.Slot0.kA = 0;
   }
-  public class HoodSimConstants {
-    public static final TalonFXConfiguration simConfiguration = new TalonFXConfiguration();
 
-    static {
-
-      // peaks:
-      simConfiguration.CurrentLimits.StatorCurrentLimitEnable = true;
-      simConfiguration.CurrentLimits.StatorCurrentLimit = STATOR_CURRENT;
-      simConfiguration.CurrentLimits.StatorCurrentLimitEnable = true;
-      simConfiguration.CurrentLimits.SupplyCurrentLimit = SUPPLY_CURRENT;
-
-      // settings
-      simConfiguration.Feedback.SensorToMechanismRatio = GEAR_RATIO;
-      simConfiguration.MotorOutput.NeutralMode = NEUTRAL_MODE;
-      simConfiguration.MotorOutput.Inverted = INVERTED;
-
-      // PID
-      simConfiguration.Slot0.kP = 10; // TODO: tune
-      simConfiguration.Slot0.kI = 0.0;
-      simConfiguration.Slot0.kD = 1;
-
-      // feedforward
-      simConfiguration.Slot0.kS = 0; // TODO: tune
-      simConfiguration.Slot0.kV = 0;
-      simConfiguration.Slot0.kA = 0;
-    }
     public class ClimbSimConstants {
-    public static final TalonFXConfiguration simConfiguration = new TalonFXConfiguration();
+      public static final TalonFXConfiguration simConfiguration = new TalonFXConfiguration();
 
-    static {
+      static {
 
-      // peaks:
-      simConfiguration.CurrentLimits.StatorCurrentLimitEnable = true;
-      simConfiguration.CurrentLimits.StatorCurrentLimit = STATOR_CURRENT;
-      simConfiguration.CurrentLimits.StatorCurrentLimitEnable = true;
-      simConfiguration.CurrentLimits.SupplyCurrentLimit = SUPPLY_CURRENT;
+        // peaks:
+        simConfiguration.CurrentLimits.StatorCurrentLimitEnable = true;
+        simConfiguration.CurrentLimits.StatorCurrentLimit = STATOR_CURRENT;
+        simConfiguration.CurrentLimits.StatorCurrentLimitEnable = true;
+        simConfiguration.CurrentLimits.SupplyCurrentLimit = SUPPLY_CURRENT;
 
-      // settings
-      simConfiguration.Feedback.SensorToMechanismRatio = GEAR_RATIO;
-      simConfiguration.MotorOutput.NeutralMode = NEUTRAL_MODE;
-      simConfiguration.MotorOutput.Inverted = INVERTED;
+        // settings
+        simConfiguration.Feedback.SensorToMechanismRatio = GEAR_RATIO;
+        simConfiguration.MotorOutput.NeutralMode = NEUTRAL_MODE;
+        simConfiguration.MotorOutput.Inverted = INVERTED;
 
-      // PID
-      simConfiguration.Slot0.kP = 10; // TODO: tune
-      simConfiguration.Slot0.kI = 0.0;
-      simConfiguration.Slot0.kD = 1;
+        // PID
+        simConfiguration.Slot0.kP = 10; // TODO: tune
+        simConfiguration.Slot0.kI = 0.0;
+        simConfiguration.Slot0.kD = 1;
 
-      // feedforward
-      simConfiguration.Slot0.kS = 0; // TODO: tune
-      simConfiguration.Slot0.kV = 0;
-      simConfiguration.Slot0.kA = 0;
+        // feedforward
+        simConfiguration.Slot0.kS = 0; // TODO: tune
+        simConfiguration.Slot0.kV = 0;
+        simConfiguration.Slot0.kA = 0;
+      }
+      public static final TalonFXSimState simMotor = _motor.getSimState();
+
+      //sim config
+      public static double WEIGHT = 50; // TODO: find
+      public static double RADIUS = 50; // TODO: find
+      public static double MAXHEIGHT = 50; // TODO: find
+      public static boolean DOGRAVITY = true; // TODO: decide
+
+      static public DCMotor motorModel = DCMotor.getFalcon500(2);
+      public static ElevatorSim elevatorSim = new ElevatorSim(motorModel,
+          GEAR_RATIO,
+          WEIGHT,
+          RADIUS,
+          0,
+          MAXHEIGHT,
+          DOGRAVITY,
+          0);
     }
-    public static final TalonFXSimState simMotor = _motor.getSimState();
-
-     public static final  armSim =
-}
+  }
